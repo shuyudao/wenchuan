2bc1491d0f6d0c9ca8ce312aa0b1bf253bb339ce
王杰 <31978102+w2423458891@users.noreply.github.com>
2019-03-04 18:50:47 +0800

    Update config.php

--- config.php
+++ config.php
@@ -16,10 +16,10 @@ global $bucket;
 global $qiniuUrl;
 global $is_delqiniu;
 
-$accessKey = "WMOuej-P_84LO4bKrsuNSstudJ0BOMBOCbAFLtmz";  //七牛云公钥
-$secretKey = 'hfAtmg3TPm4cf1qJfql8Qa7f_1u3dB78HsSjmpgM';	//七牛云私钥
-$bucket ='yun66'; //七牛云空间
-$qiniuUrl = 'http://beifen.hm115.cn';  //七牛云加速域名  不要以 ‘/’ 结尾
+$accessKey = "";  //七牛云公钥
+$secretKey = '';	//七牛云私钥
+$bucket =''; //七牛云空间
+$qiniuUrl = '';  //七牛云加速域名  不要以 ‘/’ 结尾
 $pwd = '233233'; //内容清理密码  清理已被下载标记的内容
 $is_delqiniu = '1';  //1删除数据库记录与七牛云文件  0 仅仅删除数据记录
 ?>
